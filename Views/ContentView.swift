2f04c850834c6f88f723f42409af3ed6635c3ddd
Kevin McEwan <mackevinprogramming@gmail.com>
2022-11-17 10:45:59 -0800

    initial commit

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -13,10 +13,23 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            WelcomeView()
-                .environmentObject(locationManager)
+            
+            // An if statement to show cordinates of user but if statement bc sharing coordinates is optional
+            if let location = locationManager.location {
+                // print out long and lat cord
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            } // If coords are given we will show a progress bar but if no coords then we will just show the regular 'Welcome Screen'
+                else {
+                if locationManager.isLoading {
+                    // Load 'ProgressView' from LoadingView Script
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
         }
-        .background(Color(hue: 0.744, saturation: 0.692, brightness: 0.888))
+        .background(Color(hue: 0.744, saturation: 0.596, brightness: 0.396))
         .preferredColorScheme(.dark)
     }
 }
